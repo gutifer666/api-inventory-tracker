0a4e3090bbd35e96fbffe992209be85b40477e6a
Javier <gutifer666@gmail.com>
2025-02-25 19:11:49 +0100

    Init Commit

---
+++ src/main/java/com/javiergutierrez/inventory_tracker_api/InventoryTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.javiergutierrez.inventory_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javiergutierrez/inventory_tracker_api/InventoryTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javiergutierrez.inventory_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
